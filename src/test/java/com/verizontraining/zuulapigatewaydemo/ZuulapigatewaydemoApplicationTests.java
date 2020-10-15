da93caa3aa2a16df505ccb34492a4ae40833a166
Programming Wheels <programmingwheels@gmail.com>
2020-10-15 18:27:22 +0530

    first commit

---
+++ src/test/java/com/verizontraining/zuulapigatewaydemo/ZuulapigatewaydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verizontraining.zuulapigatewaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulapigatewaydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
